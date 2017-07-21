3c90eba7408cbcd9d9354f7d51152dd79b32358f
abhishek upadhyay <Abhishekupadhyay153@gmail.com>
2017-07-22 03:22:16 +0530

    intial commit

---
+++ app/src/main/java/com/example/au/aichatbot/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.example.au.aichatbot;
+
+/**
+ * Created by Abhishek upadhyay on 16-09-2016.
+ */
+public class ChatMessage {
+    public boolean left;
+    public String message;
+
+    public ChatMessage(boolean left, String message) {
+        super();
+        this.left = left;
+        this.message = message;
+    }
+}
---
+++ app/src/main/java/com/example/au/aichatbot/MainActivity.java
@@ -0,0 +1,116 @@
+package com.example.au.aichatbot;
+
+import android.database.DataSetObserver;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AbsListView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+ private Button buttonSend;
+    private ListView listView;
+    private EditText chatText;
+    private ChatArrayAdapter chatArrayAdapter;
+    private boolean side = false;
+    private Aichat aichat=new Aichat();;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        buttonSend = (Button) findViewById(R.id.buttonSend);
+
+        listView = (ListView) findViewById(R.id.listView1);
+
+         chatArrayAdapter = new ChatArrayAdapter(getApplicationContext(), R.layout.chatbox);
+        listView.setAdapter(chatArrayAdapter);
+
+        chatText = (EditText) findViewById(R.id.chatText);
+        chatText.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    try {
+                        return sendChatMessage();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                return false;
+            }
+        });
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                try {
+                    sendChatMessage();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        listView.setTranscriptMode(AbsListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
+        listView.setAdapter(chatArrayAdapter);
+
+        //to scroll the list view to bottom on data change
+        chatArrayAdapter.registerDataSetObserver(new DataSetObserver() {
+            @Override
+            public void onChanged() {
+                super.onChanged();
+                listView.setSelection(chatArrayAdapter.getCount() - 1);
+            }
+        });
+
+    }
+
+    private boolean sendChatMessage() throws Exception {
+        chatArrayAdapter.add(new ChatMessage(side, chatText.getText().toString()));
+       String reply= aichat.get_input(chatText.getText().toString());
+       chatText.setText("");
+        side = !side;
+        getAnswer(reply);
+        return true;
+    }
+    private boolean getAnswer(String reply) throws Exception {
+        chatArrayAdapter.add(new ChatMessage(side,reply));
+
+
+        side = !side;
+        return true;
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
